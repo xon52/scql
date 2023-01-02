a9dceb85d339000c73409f479836e73ca4d446cb
xon52 <30713944+xon52@users.noreply.github.com>
2023-01-03 07:46:17 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout>
+    <v-app-bar color="grey-lighten-2"></v-app-bar>
+    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+    <v-main>
+      <v-card height="200px"></v-card>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts"></script>
